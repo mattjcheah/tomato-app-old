c9237ebd2c56b3823b55b971f01f64a8767e57da
infinitepure <matt.j.cheah@gmail.com>
2017-11-16 00:03:02 +1100

    Create models and migrate to database

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Tomato(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    duration = models.DurationField()
+    task = models.CharField(max_length=50)
+    day = models.DateField()
