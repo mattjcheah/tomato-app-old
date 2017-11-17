81f0e5ab1f7c39a3d4b489750d1981f4b872b29a
infinitepure <matt.j.cheah@gmail.com>
2017-11-18 00:32:03 +1100

    Create new countdown page with url map, view and template

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -7,4 +8,6 @@ def index(request):
         return render(request, 'index.html')
     return render(request, 'tracker/tracker.html')
 
-
+@login_required
+def countdown(request):
+    return render(request, 'tracker/countdown.html')
