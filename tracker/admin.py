3496a0eff90bd9ccba68dfc20078b209bf5f95f0
infinitepure <matt.j.cheah@gmail.com>
2017-11-18 00:33:44 +1100

    Register models with admin site

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,6 @@
+from .models import Profile, Deadline, Tomato
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Deadline)
+admin.site.register(Tomato)
